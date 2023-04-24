4a1bf45e461105c21e68b171b45656c40df94198
Gustavo Sotres <gus.sotres@gmail.com>
2023-04-24 14:25:17 -0600

    Added TODO note

--- frontend/src/utils/deck_creation.py
+++ frontend/src/utils/deck_creation.py
@@ -1,5 +1,7 @@
 from src.utils.consts import Color, Value
 
+# TODO: This should be moved to the deck class since its only used when we create a deck
+
 
 def all_card_colors() -> list[Color]:
     """Returns a list with the colors we want to have in the deck"""
