795470ebfd55218381088f784a8b7504d57f9fdf
Gustavo Sotres <gus.sotres@gmail.com>
2023-04-24 22:55:01 -0600

    Added docstring

--- frontend/src/card_container/card_container.py
+++ frontend/src/card_container/card_container.py
@@ -8,26 +8,28 @@ from src.card.card import Card
 
 
 class CardContainer(ABC):
+    """"Abstract class used to create other card containers"""
 
     def __init__(self):
         self._cards: List[Card] = list()
 
     @property
     def is_empty(self) -> bool:
+        """Is the card container empty"""
         return len(self._cards) == 0
 
     @abstractmethod
     def add(self, card: Card) -> None:
-        pass
+        """TBD"""
 
     @abstractmethod
     def get(self, index: int) -> Card:
-        pass
+        """TBD"""
 
     @abstractmethod
     def get_all(self) -> List[Card]:
-        pass
+        """TBD"""
 
     @abstractmethod
     def delete(self, card: Card) -> None:
-        pass
+        """TBD"""
